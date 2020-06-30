d28670048184ad8b1d73e1a7106057867a0f4732
bumhee lee <hufs.dlqjagml6447@naver.com>
2020-06-30 18:37:09 +0900

    자바코드 작성

--- app/src/main/java/com/professionalandroid/apps/memo/bum.java
+++ app/src/main/java/com/professionalandroid/apps/memo/bum.java
@@ -16,6 +16,17 @@ public class bum extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnsave = findViewById(R.id.btn_save);
+        btnsave.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                final TextView text_save = findViewById(R.id.text_save);
+                final EditText edit_edit = findViewById(R.id.edit_edit);
+                text_save.setText(edit_edit.getText());
+
+
+            }
+        });
 
 
         
